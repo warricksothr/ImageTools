b8f9b05281d3a3219ed59c43c9277bf14b040810
Drew Short <drew@linux-dw7n.site>
2013-12-31 20:16:27 -0600

    Updated logging to be more concise and added a failover for no logging file existing.

--- src/main/java/com/sothr/imagetools/App.java
+++ src/main/java/com/sothr/imagetools/App.java
@@ -1,13 +1,49 @@
 package com.sothr.imagetools;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        File file = new File("log4j.properties");
+        if (file.exists()) {
+            PropertyConfigurator.configure("log4j.properties");
+        } else {
+            //Simple error logging configuration
+            Properties defaultProps = new Properties();
+            defaultProps.setProperty("log4j.rootLogger","ERROR, A1");
+            //Rolling Error logger
+            defaultProps.setProperty("log4j.appender.A1","org.apache.log4j.RollingFileAppender");
+            defaultProps.setProperty("log4j.appender.A1.File","Image-Tools.err");
+            defaultProps.setProperty("log4j.appender.A1.MaxFileSize","100KB");
+            defaultProps.setProperty("log4j.appender.A1.MaxBackupIndex","1");
+            defaultProps.setProperty("log4j.appender.A1.layout","org.apache.log4j.EnhancedPatternLayout");
+            defaultProps.setProperty("log4j.appender.A1.layout.ConversionPattern","%d{yy-MM-dd HH:mm:ss} %-5p [%c{3.}] - %m%n");
+            PropertyConfigurator.configure(defaultProps);
+        }
+
+        logger.info("Image-Tools is starting");
+
+        try {
+            System.out.println( "Hello World!" );
+        } catch (Exception ex) {
+            logger.error("A fatal error has occurred: ", ex);
+            //show popup about the error to the user then exit
+        }
+
+        logger.info("Image-Tools is shutting down");
     }
 }
