13386abba463f531f61bb376b0e9f1c3554bc1a0
Drew Short <nerian.drew@gmail.com>
2014-01-02 16:46:49 -0500

    Implemented a simple package script to be run in the head directory.

--- package.sh
+++ package.sh
@@ -1,2 +1,13 @@
-#Build and package the application for distribution
+#Getting variables that are produced by the script
+VERSION=$(head -1 ./version.info)
+NAME=$(head -1 ./name.info)
+PACKAGENAME=$NAME-$VERSION
+PACKAGETAR=$PACKAGENAME.tar.gz
+PACKAGEZIP=$PACKAGENAME.zip
+TARGET=$PWD/target
 
+cd target/build/
+echo "creating $PACKAGETAR in $TARGET"
+tar -zcvf $TARGET/$PACKAGETAR .
+echo "creating $PACKAGEZIP in $TARGET"
+zip -r $TARGET/$PACKAGEZIP ./*
