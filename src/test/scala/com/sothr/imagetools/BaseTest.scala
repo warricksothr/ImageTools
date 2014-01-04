99845200c860bb51916ae94ab9b309e4c04f671e
Drew Short <nerian.drew@gmail.com>
2014-01-04 11:00:54 -0500

    Hopefully have this testing stuff sorted now :)

---
+++ src/test/scala/com/sothr/imagetools/BaseTest.scala
@@ -0,0 +1,5 @@
+package com.sothr.imagetools
+
+import org.scalatest.{FunSuite,Matchers,OptionValues,Inside,Inspectors}
+
+abstract class BaseTest extends FunSuite with Matchers with OptionValues with Inside with Inspectors
