0fad91bf4ee737fca1f8f5888679e96e4189b89f
Drew Short <nerian.drew@gmail.com>
2014-01-26 08:20:29 -0600

    Added RELEASE tag script and updated documentation

---
+++ changeTagToRelease.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Update the release version of the project
+#version string will look similar to this: 0.1.0-DEV-27-060aec7
+VERSION=$(head -1 ./version.info)
+
+#do work on the version to get the correct info
+#we need the version string from above to look like this: 0.1.1-DEV
+IFS='.' read -a arr <<< "$VERSION"
+#results in [0,1,0-DEV-27-060aec7]
+IFS='-' read -a arr2 <<< "${arr[2]}"
+#results in [0,DEV,27,060aec7]
+VERSION="${arr[0]}.${arr[1]}.${arr2[0]}-RELEASE"
+#echo $VERSION
+
+#update the POM
+mvn versions:set -DnewVersion=$VERSION
+
+echo "$VERSION" > version.info
