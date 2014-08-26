6930ac3af10c60197c7539fe09074edc1804d643
Drew Short <drew@sothr.com>
2014-08-26 15:27:52 -0500

    Fixed A shell script that I had modified but accidentally restored from a previous commit. Working on getting versioning working again

--- build/linux/package.sh
+++ build/linux/package.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 #Move out of the build folder
 cd ../..
+
+packageProject ()
+{
+    VERSION=$1
+    RELEASE=$3
+    ROOT=$PWD
+    cd $2
+
+    #Getting variables that are produced by the script
+    NAME=$(head -1 ./name.info)
+    PACKAGENAME=$NAME-$VERSION
+    PACKAGETAR=$PACKAGENAME.tar.gz
+    PACKAGEZIP=$PACKAGENAME.zip
+    TARGET=$PWD/target
+
+    #Packaging Jar Distributable
+    cd target/release/
+    echo "creating $PACKAGETAR in $TARGET"
+    tar -zcvf $RELEASE/$PACKAGETAR .
+    echo "creating $PACKAGEZIP in $TARGET"
+    zip -r $RELEASE/$PACKAGEZIP ./*
+    #Removing prebuilt jar in target
+    rm $TARGET/*.jar
+    #Copying Jar to target for archiving
+    cp ./*.jar $TARGET
+
+    cd $ROOT
+}
+
 #Getting variables that are produced by the script
 VERSION=$(head -1 ./version.info)
-NAME=$(head -1 ./name.info)
-PACKAGENAME=$NAME-$VERSION
-PACKAGETAR=$PACKAGENAME.tar.gz
-PACKAGEZIP=$PACKAGENAME.zip
-TARGET=$PWD/target
+RELEASE="$PWD/target"
 
-#Packaging Jar Distributable
-cd target/release/
-echo "creating $PACKAGETAR in $TARGET"
-tar -zcvf $TARGET/$PACKAGETAR .
-echo "creating $PACKAGEZIP in $TARGET"
-zip -r $TARGET/$PACKAGEZIP ./*
-#Removing prebuilt jar in target
-rm $TARGET/*.jar
-#Copying Jar to target for archiving
-cp ./*.jar $TARGET
+#make sure release exists
+if [ ! -d $RELEASE ]; then
+    mkdir $RELEASE
+fi
+
+packageProject $VERSION "./cli" $RELEASE
+packageProject $VERSION "./gui" $RELEASE
+#packageProject $VERSION "./daemon" $RELEASE
